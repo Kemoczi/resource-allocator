e48df863754d029958bc3b0145085994ae48c9e3
Tomek <tomcraves@gmail.com>
2026-04-04 23:49:13 +0200

    Starting

---
+++ app/main.py
@@ -0,0 +1,15 @@
+
+# app = FastAPI()
+#
+#
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+#
+# @app.get("/elo")
+# async def elo():
+#     return {"message": "No elo ryju"}
+#
+# @app.post("/interface/{param}")
+# async def assign_interface(param):
+#     return {"message": f"Interface with {param} assigned"}
