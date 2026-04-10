fbeb1df550251785ae8383c925d4811f6e0f6414
Tomek <tomcraves@gmail.com>
2026-04-10 09:30:13 +0200

    Time measurement fixture added to test_client testing

--- test/test_resource_allocator.py
+++ test/test_resource_allocator.py
@@ -1,22 +1,25 @@
 
-def test_root(test_client):
-    response = test_client.get("/")
+def test_root(test_client, check_time):
+    with check_time():
+        response = test_client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, this is resource allocator! ;)"}
 
 
-def test_resources(test_client):
-    response = test_client.get("/resources/")
+def test_resources(test_client, check_time):
+    with check_time():
+        response = test_client.get("/resources/")
     data = response.json()
 
     assert response.status_code == 200
     assert len(data) == 10
 
 
-def test_assign_interface_by_speed(test_client, create_payload):
+def test_assign_interface_by_speed(test_client, create_payload, check_time):
     payload = create_payload(phy_speed="10G")
 
-    response = test_client.post("/resources/assign-interface", json=payload)
+    with check_time():
+        response = test_client.post("/resources/assign-interface", json=payload)
     data = response.json()
 
     assert response.status_code == 200
@@ -24,17 +27,19 @@ def test_assign_interface_by_speed(test_client, create_payload):
     assert data["assigned"] is True
 
 
-def test_check_reserved(test_client, create_payload):
+def test_check_reserved(test_client, create_payload, check_time):
     payload = create_payload(optics="100GBASE-ER")
 
-    response = test_client.post("/resources/assign-interface", json=payload)
+    with check_time():
+        response = test_client.post("/resources/assign-interface", json=payload)
     data = response.json()
 
     assert response.status_code == 200
     assert data["phy_speed"] == "100G"
     assert data["assigned"] is True
 
-    response = test_client.post("/resources/assign-interface", json=payload)
+    with check_time():
+        response = test_client.post("/resources/assign-interface", json=payload)
     data = response.json()
     assert response.status_code == 404
     assert data == {"detail": "No resources with specified parameters available"}
